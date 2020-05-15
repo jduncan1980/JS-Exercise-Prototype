35014dffe90c4fd14b020b2650e99513cac5fe13
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-05-14 21:05:19 -0400

    removed unnecessary code

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ Car.prototype.drive = function (distance) {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby(name, age, favoriteToy) {
-	Person.call(this, name, age, favoriteToy);
+	Person.call(this, name, age);
 	this.favoriteToy = favoriteToy;
 }
 Baby.prototype = Object.create(Person.prototype);
